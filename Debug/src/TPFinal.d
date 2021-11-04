f6394cc6b0ea3ab1097708fba091ab2e7e0fc724
tanterajulian <tanterajulian@gmail.com>
2021-11-04 19:49:19 -0300

    codigo implementado

---
+++ Debug/src/TPFinal.d
@@ -0,0 +1,45 @@
+src/TPFinal.o src/TPFinal.d: ../src/TPFinal.c \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\inc/LPC17xx.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\inc/core_cm3.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\inc/core_cmInstr.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\inc/core_cmFunc.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpio.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc_types.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_pinsel.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\inc/lpc17xx.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_exti.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_systick.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_timer.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_uart.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/lcd.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/stdutils.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/gpio.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/delay.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/lcd.c \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/delay.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/lcd.h \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/delay.c \
+ C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/gpio.c
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\inc/LPC17xx.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\inc/core_cm3.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\inc/core_cmInstr.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\inc/core_cmFunc.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpio.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc_types.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_pinsel.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\inc/lpc17xx.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_exti.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_systick.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_timer.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_uart.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/lcd.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/stdutils.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/gpio.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/delay.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/lcd.c:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/delay.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/lcd.h:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/delay.c:
+C\:\Users\tante\Documents\MCUXpressoIDE_11.4.0_6224\driversWorkspace\TPFinal\inc/gpio.c:
